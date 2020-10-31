bfdec4a6606e152c71b0f09c96b15b0d37c2226a
Rokibul Islam <rokibul630@gmail.com>
2020-10-31 20:12:56 +0530

    add basic pay calculator

---
+++ utils/calc_basic_pay.py
@@ -0,0 +1,6 @@
+
+def cal_basic_pay(basicpay_widget):
+    # tempbpension = tempbpension_arg
+    tempbpension = int(basicpay_widget.get())
+    tempbpension = tempbpension/2
+    return tempbpension
